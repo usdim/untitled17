86a6044ce0d77222612b7cec6af4f42f5095d7f4
usdim <csnlabs@hotmail.com>
2024-10-09 19:30:01 +0300

    Ошибки

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        int[] st = {-10, 0, -10, 0, -10};
-        int c = 0;
-        int m = 0;
+        Scanner in = new Scanner(System.in);            // объявление переменной, что вводится с клавиатуры
+        System.out.println("Введите число");        // текст введите число
+        int N = in.nextInt();
+        int[] massiv = new int[N];
 
-        for (int i = 0; i < st.length; i++) {
-            if (st[i] < 1) { c = 0;
-                            }
-            else c=c+1;
-
-                if (c > m) {
-                m = c;
-            }
-        }
-        System.out.println(m);
+        System.out.println(calcMaxDays(max));
     }
+
+    public static int calcMaxDays(int massiv[]) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i] < 1) {
+                count = 0;
+            } else count = count + 1;
+
+            if (count > max) {
+                max = count;
+                return max;
+            }
+
+        }
+
+
+    }
+
+
 }
+
+
+
+
